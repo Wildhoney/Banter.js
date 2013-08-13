fb6345e4478d3964e953eb49e103b758b0d9037d
Adam Timberlake <adam.timberlake@gmail.com>
2013-08-13 21:38:54 +0100

    Added basic API. Relates to #2

---
+++ api/default.rb
@@ -0,0 +1,12 @@
+require_relative 'vendor/Ruby-IRC/lib/IRC.rb'
+require_relative 'vendor/em-websocket/lib/em-websocket.rb'
+
+# Initialise the WebSocket on port 8080.
+EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
+
+  # Initial debug information for the API.
+  ws.onopen    { ws.send "Hello Client!"}
+  ws.onmessage { |msg| ws.send "Pong: #{msg}" }
+  ws.onclose   { puts "WebSocket closed" }
+
+end
