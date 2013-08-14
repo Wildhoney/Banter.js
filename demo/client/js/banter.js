461720a6fa35a94ed9b449f0f6d9bc274b98fc34
Adam Timberlake <adam.timberlake@gmail.com>
2013-08-15 00:13:04 +0100

    Frontend Angular and backend Ruby now communicate with one another

--- demo/client/js/banter.js
+++ demo/client/js/banter.js
@@ -1,6 +1,43 @@
-(function() {
+window.banterApp = angular.module('banterApp', []);
 
-    window.banterApp = angular.module('banterApp', []);
+/**
+ * @controller ApplicationController
+ * @param $scope {Object}
+ * @constructor
+ */
+function ApplicationController($scope) {
 
+    var socket = new WebSocket('ws://localhost:8080');
 
-})();
+    socket.onopen = function(event) {
+        socket.send('Test baby');
+        $scope.connected = true;
+        $scope.$apply();
+    };
+
+    socket.onmessage = function(event) {
+        console.log(event.data);
+    };
+
+    /**
+     * @property message
+     * @type {String}
+     */
+    $scope.message = '';
+
+    /**
+     * @method sendMessage
+     * @param message {String}
+     * @return {Boolean}
+     */
+    $scope.sendMessage = function sendMessage(message) {
+
+        // Send the message to the awaiting WebSocket server!
+        return socket.send(JSON.stringify({
+            name: 'Wildhoney',
+            message: message }
+        ));
+
+    }
+
+}
--- demo/server/default.rb
+++ demo/server/default.rb
@@ -2,6 +2,9 @@
 $:.unshift File.dirname(__FILE__).concat('/vendor/Ruby-IRC/lib/')
 $:.unshift File.dirname(__FILE__).concat('/vendor/em-websocket/lib/')
 
+# Our native dependencies.
+require 'json'
+
 # Now both libraries are in the $LOAD_PATH ($:) we can load them.
 require_relative 'vendor/Ruby-IRC/lib/IRC.rb'
 require_relative 'vendor/em-websocket/lib/em-websocket.rb'
@@ -10,18 +13,18 @@ require_relative 'vendor/em-websocket/lib/em-websocket.rb'
 require_relative 'lib/default'
 
 # Initialise the WebSocket on port 8080.
-EventMachine::WebSocket.start(:host => '0.0.0.0', :port => 8080) do |ws|
+EventMachine::WebSocket.start(:host => '127.0.0.1', :port => 8080) do |ws|
 
   ws.onopen {
     puts "WebSocket connection open"
 
     # publish message to the client
-    ws.send "Hello Client"
+    ws.send({ :name => 'Adam', :message => 'Test...' }.to_json)
   }
 
   ws.onclose { puts "Connection closed" }
   ws.onmessage { |msg|
-    puts "Recieved message: #{msg}"
+    puts "Received message: #{msg}"
     ws.send "Pong: #{msg}"
   }
 
